403c2658c3f4567c215e98f56880b8bfb7f3df4d
rParslow <iorek@m4p.li>
2015-09-09 01:37:03 +0200

    add filter

--- list.php
+++ list.php
@@ -3,11 +3,32 @@
 // Instantiate Class
 //
 include_once('classes/tbs_class.php');
+
+// Filters
 //
+$siteId   = (!empty($_GET["siteId"])) ? $_GET["siteId"] : NULL;
+$search   = (!empty($_GET["search"])) ? $_GET["search"] : NULL;
+
+// Build condition 
+//
+$where='';
+if (!empty($siteId)) {
+  $where .= " vads_site_id = '$siteId'";
+}
+if (!empty($search)) {
+  if (!empty($where)) $where .= ' AND ';
+  $where .= "( vads_cust_email = '$search' OR vads_trans_uuid = '$search' OR vads_order_id = '$search' )";
+}
+if (!empty($where)) $where = " WHERE $where ";
+
+
 // Grep data from database
 //
 $db   = new SQLite3("./database/payzen.sqlite");
-$results = $db->query('SELECT * FROM ipn');
+$results = $db->query("SELECT * FROM ipn $where");
+
+
+
 //
 // Prepare array for TBS template
 //
@@ -19,6 +40,8 @@ while ($res = $results->fetchArray()) {
   $res['full'] = json_encode($wrk, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
   $data[] = $res;
 }
+
+
 //
 // Ask TBS to display data
 //
