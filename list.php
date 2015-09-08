85c897c32c90a81613051347f716e5e829bd6db4
rParslow <iorek@m4p.li>
2015-09-08 17:32:10 +0200

    IPN tables visualizer
    Using Tiny but Strong

---
+++ list.php
@@ -0,0 +1,28 @@
+<?php
+//
+// Instantiate Class
+//
+include_once('classes/tbs_class.php');
+//
+// Grep data from database
+//
+$db   = new SQLite3("./database/payzen.sqlite");
+$results = $db->query('SELECT * FROM ipn');
+//
+// Prepare array for TBS template
+//
+while ($res = $results->fetchArray()) {
+  $res['vads_payment_seq'] = json_encode(json_decode($res['vads_payment_seq']), JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
+  #$res['full'] = json_encode(json_decode($res['full']), JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
+  $wrk = json_decode($res['full'],true);
+  if ($wrk['vads_payment_seq'] != '' ) $wrk['vads_payment_seq'] = "<code>=>  MULTI</code>";
+  $res['full'] = json_encode($wrk, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
+  $data[] = $res;
+}
+//
+// Ask TBS to display data
+//
+$TBS = new clsTinyButStrong;
+$TBS->LoadTemplate('tpl_ipn.html');
+$TBS->MergeBlock('list',$data);
+$TBS->Show();
