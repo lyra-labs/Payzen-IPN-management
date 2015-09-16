690c8216e1c7e13997e7e905beb644a3f9c1d6db
rParslow <iorek@m4p.li>
2015-09-16 19:10:42 +0200

    change the ipn table to manage most of the information in feilds

--- classes/classes.inc.php
+++ classes/classes.inc.php
@@ -20,22 +20,58 @@ function create_ipn_table() {
     ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     status STRING, 
     vads_site_id STRING, 
-    vads_shop_name STRING, 
     vads_url_check_src STRING,
+    vads_payment_src STRING,
+    vads_shop_name STRING, 
     vads_ctx_mode STRING, 
     vads_trans_uuid STRING, 
-    vads_order_id STRING, 
+    vads_order_id STRING,
+    vads_order_info STRING,
+    vads_payment_config STRING, 
     vads_effective_creation_date DATETIME, 
     vads_operation_type STRING, 
-    vads_trans_status STRING, 
+    vads_trans_status STRING,
+    vads_result STRING,
+    vads_extra_result STRING,
+ 
     vads_effective_amount STRING, 
-    vads_currency STRING, 
+    vads_currency STRING,
+    vads_contract_used STRING, 
     vads_auth_mode STRING, 
     vads_card_brand STRING, 
     vads_card_number STRING, 
     vads_cust_email STRING, 
+
+    vads_capture_delay STRING,
+    vads_presentation_date STRING,
+    vads_warranty_result STRING,
+    vads_risk_control STRING,
+
+    vads_validation_mode STATUS,
+    vads_recurrence_status STRING,
+
+    vads_identifier_status STRING,
+    vads_identifier STRING,
+
+    vads_subscription STRING,
+    vads_sub_desc STRING,
+    vads_sub_effect_date STRING,
+    vads_sub_currency STRING,
+    vads_sub_amount STRING,
+    vads_sub_init_amount_number STRING,
+    vads_sub_init_amount STRING,
+
+    vads_contrib STRING,
+
     vads_payment_seq BLOB, 
-    full BLOB, 
+
+    vads_ext_info_donation STRING,
+    vads_ext_info_donation_recipient STRING,
+    vads_ext_info_donation_recipient_name STRING,
+    vads_ext_info_donation_merchant STRING,
+
+    full BLOB,
+ 
     signature STRING, 
     checked STRING
     )"
